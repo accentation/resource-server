96c1bd28bd279e34f20c8b2bda0d9e9d5d4a20dd
javigs82 <javigs82@gmail.com>
2017-05-08 17:35:34 +0200

    Initial Commit

---
+++ src/main/java/com/accenture/banking/config/SpringApplicationBuilder.java
@@ -0,0 +1,12 @@
+package com.accenture.banking.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationBuilder {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationBuilder.class, args);
+	}
+}
