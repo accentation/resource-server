86c070f16d5474608108f428d5a6ccb27eeeead4
javigs82 <javigs82@gmail.com>
2017-05-17 15:12:50 +0200

    Addind tests suffix in test class in order to be executed by spring boot

--- src/test/java/com/accenture/banking/TestApplicationBuilderTests.java
+++ src/test/java/com/accenture/banking/TestApplicationBuilderTests.java
@@ -1,21 +1,19 @@
 package com.accenture.banking;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-public class TestApplicationBuilder {
+public class TestApplicationBuilderTests {
 
 	@Autowired
 	TestRestTemplate restTemplate;
